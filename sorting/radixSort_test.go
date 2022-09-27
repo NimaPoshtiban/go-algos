380423a42460291947a4f3f5839f2d2ce238a4e1
Nima Poshtiban <Poshtibannima@gmail.com>
2022-09-27 19:07:28 +0330

    add radix sort

---
+++ sorting/radixSort_test.go
@@ -0,0 +1,21 @@
+package sorting
+
+import (
+	"math/rand"
+	"sort"
+	"testing"
+	"time"
+)
+
+func TestRadixSort(t *testing.T){
+	rand.Seed(time.Now().UnixNano())
+	for i := 0; i < len(list); i++ {
+		list[i] = rand.Intn(100)
+	}
+	sortedList := RadixSort(list)
+	if !sort.IntsAreSorted(sortedList){
+		t.Log(list)
+		t.Errorf("Slice is not sorted")
+	}
+	t.Log(list)
+}
